5a337c81ff0107de6f5e93c8eae1fc48b9135347
spencer <spencajones@gmail.com>
2018-09-01 12:10:23 -0600

    added css grid for layout of home page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,34 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div>
-        Future Code Here...
-        {/* Header Here */}
-        {/* Nav Menu Here*/}
-        {/* Home Page */}
-        {/* Footer Here */}
+      <div style={styles.container}>
+        <div style={styles.header}>{ 'Header' }</div>
+        <div style={styles.nav}>{ 'Nav Menu Here'}</div>
+        <div style={styles.home}>{ 'Home Page' }</div>
+        <div style={styles.footer}>{ 'Footer Here' }</div>
       </div>
     );
   }
+}
+
+const styles = {
+  container: {
+    height: '90vh', 
+    display: 'grid',
+    grid: "'header header header' 125px 'nav nav nav' 75px 'body body body' 1fr 'footer footer footer' 150px / 1fr 4fr 1fr",
+  },
+  body: {
+    gridArea: 'body',
+  },
+  nav: {
+    gridArea: 'nav',
+  },
+  footer: {
+    gridArea: 'footer',
+  },
+  header: {
+    gridArea: 'header',
+  },
 }
 
 export default App;
