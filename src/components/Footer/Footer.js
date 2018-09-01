323263e8835252592fbab18e9304b5805e110a2b
Doug Small <dsmalz13@gmail.com>
2018-09-01 11:44:16 -0600

    html outlined

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <h2>Wheelies Inc TM</h2>
+        </div>
+        <div>
+          {/* link to careers page */}
+          <h3>We are hiring!</h3>
+          {/* link to adoptions */}
+          <h3>Adopt a pet near you</h3>
+        </div>
+      </div>
+      <div>
+        <p>
+          Wheelies come with a 1 year warranty. Warranty is not valid if used
+          with cats.
+        </p>
+      </div>
+    </div>
+  );
+};
