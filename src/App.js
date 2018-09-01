eab1ba06941687058b80b506f0c4f46f386ee4db
spencer <spencajones@gmail.com>
2018-09-01 12:25:03 -0600

    fixed body, so it has empty space on each side

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const styles = {
   container: {
     height: '90vh', 
     display: 'grid',
-    grid: "'header header header' 125px 'nav nav nav' 75px 'body body body' 1fr 'footer footer footer' 150px / 1fr 4fr 1fr",
+    grid: "'header header header' 125px 'nav nav nav' 75px '. body .' 1fr 'footer footer footer' 150px / 1fr 4fr 1fr",
   },
   body: {
     gridArea: 'body',
